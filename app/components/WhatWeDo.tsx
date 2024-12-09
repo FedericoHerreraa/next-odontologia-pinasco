0d7aad0cf1dfcea33f03d0a006090f1fc44ac103
FedericoHerreraa <fede.juan.herrera@gmail.com>
2024-12-09 15:24:40 -0300

    keep improving cards

--- app/components/WhatWeDo.tsx
+++ app/components/WhatWeDo.tsx
@@ -20,7 +20,7 @@ export const WhatWeDo = () => {
                         <p className="mx-10 mt-5 text-zinc-600">{item.description}</p>
                         <Link 
                             href='/work'
-                            className="flex items-center w-[167px] gap-2 hover:gap-5 mt-16 px-6 py-3 bg-[#00a2af] text-white font-semibold text-lg rounded-lg shadow-md transition-all duration-150"
+                            className="flex items-center w-[167px] gap-2 hover:gap-5 mt-10 px-6 py-3 bg-[#00a2af] text-white font-semibold text-lg rounded-lg shadow-md transition-all duration-150"
                         >
                             <p>Ver Más</p>
                             <IoIosArrowRoundForward size={30}/>
@@ -36,14 +36,14 @@ export const WhatWeDo = () => {
 const info = [
     {
         title: "Prevención y diagnóstico",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor, metus nec ultricies suscipit, nunc est ultrices nisi, ut"
+        description: "Incluye especialidades enfocadas en evitar problemas dentales y detectarlos de forma temprana, como limpiezas, chequeos rutinarios, aplicación de flúor, selladores y cuidado dental especializado para niños."
     },
     {
         title: "Tratamientos correctivos y restauradores",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor, metus nec ultricies suscipit, nunc est ultrices nisi, ut"
+        description: "Se centra en tratar problemas existentes para restaurar la funcionalidad y estética dental. Esto incluye tratamientos de conductos, cuidado de encías, prótesis dentales, implantes y cirugías correctivas."
     },
     {
         title: "Corrección y estética",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor, metus nec ultricies suscipit, nunc est ultrices nisi, ut"
+        description: "Agrupa procedimientos para mejorar la alineación y apariencia de los dientes, como ortodoncia, blanqueamientos, carillas y reconstrucciones dentales para lograr una sonrisa más atractiva."
     },
-]
+];
