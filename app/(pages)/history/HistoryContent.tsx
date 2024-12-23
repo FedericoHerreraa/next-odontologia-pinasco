cd31acee6c3817f674363b663e0cd8c52132702a
Federico Herrera <fede.juan.herrera@gmail.com>
2024-12-23 10:07:44 -0300

    updating history components

--- app/(pages)/history/HistoryContent.tsx
+++ app/(pages)/history/HistoryContent.tsx
@@ -13,43 +13,50 @@ export const HistoryContent = () => {
     }
 
     return (
-        <div className="relative w-full">
-            <div className="absolute top-0 bottom-0 left-1/2 w-[1px] bg-zinc-300 -translate-x-1/2"></div>
-            
-            {info.map((item, index) => (
-                <div 
-                    id={item.id.toString()} 
-                    key={index} 
-                    className={`flex ${item.id % 2 === 0 ? 'flex-row-reverse' : 'flex-row' } items-center md:gap-10 gap-5 relative mb-40`}
-                >
-                    <div className={`flex flex-col ${item.id % 2 === 0 ? 'items-end' : 'items-start'} w-1/2 gap-2`}>
-                        <h1 className={`md:text-3xl text-xl font-semibold ${bebas.className} ${item.id % 2 === 0 && 'text-end'}`}>{item.title}.</h1>
-                        <p className={`text-zinc-900 mb-5 md:text-lg ${montserrat.className}`}>Año {item.date}.</p>
-                        <p className={`md:text-base text-sm ${montserrat.className} ${item.id % 2 === 0 && 'text-end'}`}>{item.description}.</p>
-                        {item.id !== 6 && (
-                            <div className={`flex ${item.id % 2 === 0 ? 'flex-row-reverse' : 'flex-row' } gap-1 items-end md:mt-7 mt-3`}>
-                                <button 
-                                    onClick={() => navigateNext(item.id + 1)}
-                                    className="border border-zinc-300 rounded-lg md:px-4 px-2 py-1 hover:border-zinc-500 hover:bg-zinc-100 transition-all duration-200"
-                                >
-                                    <p className="md:text-base text-sm">Siguiente</p>
-                                </button>
-                                {item.id % 2 === 0 ? (
-                                    <PiArrowBendUpLeftFill size={25} className="text-zinc-500 -rotate-90"/>
-                                ) : (
-                                    <PiArrowBendUpRightFill size={25} className="text-zinc-500 rotate-90"/>
+        <div>
+            <p className={`text-center mt-10 mb-3 text-zinc-500 md:text-lg text-base ${montserrat.className}`}>Como llegamos hasta donde estamos.</p>
+            <h1 className={`md:text-6xl text-3xl text-black  text-center ${bebas.className}`}>Conoce la Historia de <span className="bg-gradient-to-r to-[#00a2af] from-[#46bcc9] bg-clip-text text-transparent">Pinasco.</span></h1>
+            <section className="mt-40 mb-20 flex flex-col gap-56 md:w-[60%] w-[95%] mx-auto">
+                <div className="relative w-full">
+                    <div className="absolute top-0 bottom-0 left-1/2 w-[1px] bg-zinc-300 -translate-x-1/2"></div>
+                    
+                    {info.map((item, index) => (
+                        <div 
+                            id={item.id.toString()} 
+                            key={index} 
+                            className={`flex ${item.id % 2 === 0 ? 'flex-row-reverse' : 'flex-row' } items-center md:gap-10 gap-5 relative mb-40`}
+                        >
+                            <div className={`flex flex-col ${item.id % 2 === 0 ? 'items-end' : 'items-start'} w-1/2 gap-2`}>
+                                <h1 className={`md:text-3xl text-xl font-semibold ${bebas.className} ${item.id % 2 === 0 && 'text-end'}`}>{item.title}.</h1>
+                                <p className={`text-zinc-900 mb-5 md:text-lg ${montserrat.className}`}>Año {item.date}.</p>
+                                <p className={`md:text-base text-sm ${montserrat.className} ${item.id % 2 === 0 && 'text-end'}`}>{item.description}.</p>
+                                {item.id !== 6 && (
+                                    <div className={`flex ${item.id % 2 === 0 ? 'flex-row-reverse' : 'flex-row' } gap-1 items-end md:mt-7 mt-3`}>
+                                        <button 
+                                            onClick={() => navigateNext(item.id + 1)}
+                                            className="border border-zinc-300 rounded-lg md:px-4 px-2 py-1 hover:border-zinc-500 hover:bg-zinc-100 transition-all duration-200"
+                                        >
+                                            <p className="md:text-base text-sm">Siguiente</p>
+                                        </button>
+                                        {item.id % 2 === 0 ? (
+                                            <PiArrowBendUpLeftFill size={25} className="text-zinc-500 -rotate-90"/>
+                                        ) : (
+                                            <PiArrowBendUpRightFill size={25} className="text-zinc-500 rotate-90"/>
+                                        )}
+                                    </div>
                                 )}
                             </div>
-                        )}
-                    </div>
 
-                    <div className="w-1/2 h-64 bg-zinc-100 rounded-lg shadow-xl">
-                    </div>
+                            <div className="w-1/2 h-64 bg-zinc-100 rounded-lg shadow-xl">
+                            </div>
 
-                    <div className="absolute left-1/2 md:w-6 w-3 md:h-6 h-3 bg-[#00a2af] rounded-full -translate-x-1/2"></div>
+                            <div className="absolute left-1/2 md:w-6 w-3 md:h-6 h-3 bg-[#00a2af] rounded-full -translate-x-1/2"></div>
+                        </div>
+                    ))}
                 </div>
-            ))}
+            </section>
         </div>
+        
     );
 };
 
--- app/(pages)/history/page.tsx
+++ app/(pages)/history/page.tsx
@@ -1,7 +1,6 @@
 
 import { Metadata } from "next";
 import { HistoryContent } from "./HistoryContent";
-import { bebas, montserrat } from "@/app/fonts/fonts"
 
 export const metadata: Metadata = {
     title: "Historia | Pinasco Odontología",
@@ -10,13 +9,7 @@ export const metadata: Metadata = {
 
 export default function History() {
     return (
-        <div>
-            <p className={`text-center mt-10 mb-3 text-zinc-500 md:text-lg text-base ${montserrat.className}`}>Como llegamos hasta donde estamos.</p>
-            <h1 className={`md:text-6xl text-3xl text-black  text-center ${bebas.className}`}>Conoce la Historia de <span className="bg-gradient-to-r to-[#00a2af] from-[#46bcc9] bg-clip-text text-transparent">Pinasco.</span></h1>
-            <section className="mt-40 mb-20 flex flex-col gap-56 md:w-[60%] w-[95%] mx-auto">
-                <HistoryContent />
-            </section>
-        </div>
+        <HistoryContent />
     )
 }
 
