5a660a2a2202009643bac8da7ecd5d731dc43cbb
Federico Herrera <fede.juan.herrera@gmail.com>
2024-12-31 11:47:15 -0300

    .

--- app/components/Location.tsx
+++ app/components/Location.tsx
@@ -4,7 +4,7 @@ import { MdLocationPin } from "react-icons/md";
 
 export const Location = () => {
     return (
-        <div id="location" className="h-[100vh] mt-40">
+        <div id="location" className="min-h-[100vh] mt-40">
             <p className={`text-center mt-10 mb-3 text-zinc-500 md:text-lg text-base ${montserrat.className}`}>Encuéntranos ahora.</p>
             <h1 className={`md:text-6xl text-4xl text-zinc-700 text-center ${bebas.className}`}>Te Mostramos Nuestra <span className="bg-gradient-to-r to-[#00a2af] from-[#46bcc9] bg-clip-text text-transparent">Ubicación.</span></h1>
 
@@ -23,7 +23,7 @@ export const Location = () => {
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1151.8013544339137!2d-58.46990501656856!3d-34.550127799144924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb7e749d648e9%3A0xb3390ff77985fc94!2sOdontolog%C3%ADa%20Pinasco!5e0!3m2!1ses!2sar!4v1735304020787!5m2!1ses!2sar" 
                     width="600" 
                     height="450" 
-                    className="md:w-1/2 rounded-md shadow-xl"
+                    className="md:w-1/2 w-[400px] rounded-md shadow-xl"
                     loading="lazy">
                 </iframe>
             </section>
