9ea5996145b4f079a958506ca51924b97013c132
Federico Herrera <fede.juan.herrera@gmail.com>
2025-01-02 19:31:38 -0300

    trying some more things to put favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
 import "./globals.css";
+import type { Metadata } from "next";
 import { Header } from "./components/Header";
 import { WhatsAppIcon } from "./components/WhatsAppIcon";
 import { MobileViewProvider } from "./context/MobileView";
@@ -8,13 +8,17 @@ import { Footer } from "./components/Footer";
 export const metadata: Metadata = {
   title: "Pinasco Odontologia",
   description: "Pinasco Odontologia es una clinica odontologica que se dedica a la salud bucal de sus pacientes.",
-  icons: {
-    icon: 'img/favicon.ico',
-  }
+  // icons: {
+  //   icon: '/img/salaEspera.jpg'
+  // },
 };
 
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ 
+  children 
+} : Readonly<{ 
+  children: React.ReactNode 
+}>) {
   return (
     <html lang="es">
       <body>
