29a2dae1283a23dc4625699868e00b8f1598572c
Federico Herrera <fede.juan.herrera@gmail.com>
2025-01-03 09:51:24 -0300

    adding hover to images in home page, changing some titles and updating some buttons

--- app/components/CallToHistory.tsx
+++ app/components/CallToHistory.tsx
@@ -5,9 +5,9 @@ import {bebas } from "@/app/fonts/fonts";
 export const CallToHistory = () => {
   return (
     <div className="md:flex md:justify-center md:items-center md:space-x-4 md:space-y-0 space-y-4 w-full h-80 shadow-md mt-40" style={{backgroundImage: `url(${img.src})`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat", opacity: 0.7}}>
-        <div className="w-100 h-auto px-6 py-6 bg-[#00a2af] text-center rounded-md shadow-xl "> 
-            <Link href="/history" className={`text-white font-thin text-3xl ${bebas.className} cursor-pointer`}>Conocé más acerca de nosotros</Link> 
-        </div>
+        <Link href="/history" className="w-100 h-auto px-6 py-6 bg-[#00a2af] text-center rounded-md shadow-xl hover:bg-[#008b96]"> 
+            <p className={`text-white font-thin text-3xl ${bebas.className}`}>Conocé más acerca de nosotros</p> 
+        </Link>
     </div>
   )
 }
