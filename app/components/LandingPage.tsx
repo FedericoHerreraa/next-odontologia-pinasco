b975a25323dc36f458d5d2378c3c87ffca3cf30b
CamilaAznar <aznarcami@gmail.com>
2025-01-29 20:21:31 -0300

    fixing landing

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -14,7 +14,7 @@ export const LandingPage = () => {
 
   return (
       <div
-        className="bg-cover bg-center md:h-[80vh] h-[70vh] flex items-center"
+        className="bg-cover bg-center md:h-[90vh] h-[70vh] flex items-center"
         style={!isMobile ? { backgroundImage: `url(${img.src})` } : {}}
       >
         <div className="md:absolute top-1/3 gap-5 mx-auto px-5 md:px-20 rounded-lg w-full max-w-7xl">
@@ -27,7 +27,7 @@ export const LandingPage = () => {
                 height={isMobile ? 100 : 130}
               />
             </div>
-            <p className={`text-zinc-500 text-sm md:text-lg mb-1 text-center md:text-left ${montserrat.className}`}>
+            <p className={`text-zinc-500 text-xs md:text-lg mb-1 text-center md:text-left ${montserrat.className}`}>
               En nuestro consultorio privado, nos especializamos en brindarte el
               <span className="font-bold"> mejor cuidado dental</span> con un enfoque personalizado y profesional.
               Nuestro equipo de odontólogos altamente capacitados utiliza
