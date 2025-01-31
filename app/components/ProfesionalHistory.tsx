09d30833dcb4f88ee38269ba74c91a30d056d868
CamilaAznar <aznarcami@gmail.com>
2025-01-31 12:21:10 -0300

    changing profesional-history

--- app/components/ProfesionalHistory.tsx
+++ app/components/ProfesionalHistory.tsx
@@ -1,86 +1,68 @@
-
-import { bebas, montserrat } from '@/app/fonts/fonts'
-import Image from 'next/image'
-import Micaela from '@/app/img/history/Micaela.png'
-import Laura from '@/app/img/history/Laura.png'
+import { bebas, montserrat } from '@/app/fonts/fonts';
+import Image from 'next/image';
+import Micaela from '@/app/img/history/Micaela.png';
+import Laura from '@/app/img/history/Laura.png';
 
 export const ProfesionalHistory = () => {
     return (
-        <div>
-            <div className="text-center mb-8">
-                <p className={`text-center mt-10 mb-3 text-zinc-500 md:text-lg text-base ${montserrat.className}`}>Conoce a nuestras profesionales.</p>
-                <h1 className={`md:text-6xl text-4xl text-black text-center md:mx-0 mx-2 ${bebas.className}`}>Nuestra historia <span className="bg-gradient-to-r to-[#00a2af] from-[#46bcc9] bg-clip-text text-transparent">Profesional</span></h1>
+        <section className="w-full flex flex-col items-center py-16 bg-gray-100">
+            <div className="text-center mb-12">
+                <p className={`text-zinc-500 text-lg ${montserrat.className}`}>Conocé a nuestras profesionales</p>
+                <h1 className={`text-5xl font-bold text-black ${bebas.className}`}>
+                    Nuestro <span className="text-[#00a2af]">Equipo</span>
+                </h1>
             </div>
 
-            <section className="flex items-center flex-col justify-center gap-10 md:w-[80%] w-[90%] mx-auto mt-20">
-                <div className="flex items-center md:flex-row flex-col-reverse md:gap-3 gap-7 bg-[#46bcc9] bg-opacity-20 shadow-xl p-5 rounded-lg">
-                    <Image
-                        src={Micaela}
-                        alt="Micaela"
-                        width={350}
-                        className="rounded-lg shadow-lg"
-                    />
-                    <p>
-                        <strong className='text-2xl'>Micaela Pinasco</strong>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro eius
-                        impedit iusto, labore eveniet officia ad delectus magnam sequi at
-                        voluptatum, cum expedita? Provident dolore deleniti harum vitae magni.
-                        Obcaecati.
-                    </p>
+            <div className="flex flex-col md:flex-row gap-12 w-[90%] md:w-[80%]">
+                <div className="flex flex-col bg-white shadow-lg overflow-hidden md:w-1/2 rounded-t-[200px]">
+                <Image 
+                    src={Micaela} 
+                    alt="Micaela Pinasco" 
+                    width={350} 
+                    height={350} 
+                    className="w-full h-[500px] object-cover rounded-t-[200px]" 
+                />
+                <div className="p-8 ">
+                    <h2 className="text-3xl font-bold text-[#00a2af] text-center">Micaela Pinasco</h2>
+                    <p className="text-gray-600 mt-4 text-base">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro eius
+                            impedit iusto, labore eveniet officia ad delectus magnam sequi at
+                            voluptatum, cum expedita? Provident dolore deleniti harum vitae magni.
+                            Obcaecati.
+                        </p>
+                    </div>
                 </div>
-                <div className="flex items-center md:flex-row flex-col md:gap-3 gap-7 text-sm bg-[#46bcc9] bg-opacity-20 shadow-xl p-5 rounded-lg">
-                    <p>
-                        <strong className='text-2xl'>Laura Pinasco</strong>
-                        <br />
-                        Odontóloga graduada de la Facultad de Odontología de la Universidad de
-                        Buenos Aires, Argentina
-                        <br />
-                        ▪ Especialista en Endodoncia, título otorgado por Ministério de Salud
-                        <br />
-                        ▪ Especialista en Endodoncia, título otorgado por la Facultad de
-                        Odontología de la Universidad de Buenos Aires
-                        <br />
-                        ▪ Docente autorizada de la Facultad de Odontología de la Universidad de
-                        Buenos Aires
-                        <br />
-                        ▪ Ex Profesora adjunta regular de la Cátedra de Endodoncia de la Facultad
-                        de Odontología de la Universidad de Buenos Aires
-                        <br />
-                        ▪ Ex Co-directora de la Carrera de Especialista en Endodoncia de la
-                        Facultad de Odontología de la Universidad de Buenos Aires
-                        <br />
-                        ▪ Ex-Coordinadora de la Unidad de Traumatismos Dentarios Facultad de
-                        Odontología de la Universidad de Buenos Aires
-                        <br />
-                        ▪ Dictante de cursos de postgrado de la F.O.U.B.A.
-                        <br />
-                        ▪ Dictante de cursos de especialidad en el país y en el exterior
-                        <br />
-                        ▪ Jurado de concursos para cargos regulares de la Facultad de Odontología
-                        de la Universidad de Buenos Aires
-                        <br />
-                        ▪ Autora de publicaciones referidas a la especialidad en revistas
-                        nacionales y en el exterior
-                        <br />
-                        ▪ Participación en campañas de extensión universitaria
-                        <br />
-                        ▪ Organizadora de PENN ENDO GLOBAL ARGENTINA 2018
-                        <br />
-                        ▪ Dictado de clases y participación académica en la Universidad
-                        Complutense de Madrid en el marco de movilidad académica internacional
-                        2018
-                        <br />
-                        ▪ Coautora en premios de trabajos de investigación presentados en el país
-                        y en el exterior
-                    </p>
-                    <Image
-                        src={Laura}
-                        alt="Laura Pinasco"
-                        width={350}
-                        className="rounded-lg shadow-lg"
-                    />
+
+                <div className="flex flex-col bg-white shadow-lg overflow-hidden md:w-1/2 rounded-t-[200px]">
+                <Image 
+                    src={Laura} 
+                    alt="Laura Pinasco" 
+                    width={350} 
+                    height={350} 
+                    className="w-full h-[500px] object-cover rounded-t-[200px]" 
+                />
+                <div className="p-8 ">
+                    <h2 className="text-3xl font-bold text-[#00a2af] text-center">Laura Pinasco</h2>
+                    <p className="text-gray-600 mt-4 text-base">
+                            ▪ Odontóloga graduada de la Facultad de Odontología de la Universidad de Buenos Aires, Argentina<br />
+                            ▪ Especialista en Endodoncia, título otorgado por Ministério de Salud<br />
+                            ▪ Especialista en Endodoncia, título otorgado por la Facultad de Odontología de la Universidad de Buenos Aires<br />
+                            ▪ Docente autorizada de la Facultad de Odontología de la Universidad de Buenos Aires<br />
+                            ▪ Ex Profesora adjunta regular de la Cátedra de Endodoncia de la Facultad de Odontología de la Universidad de Buenos Aires<br />
+                            ▪ Ex Co-directora de la Carrera de Especialista en Endodoncia de la Facultad de Odontología de la Universidad de Buenos Aires<br />
+                            ▪ Ex-Coordinadora de la Unidad de Traumatismos Dentarios Facultad de Odontología de la Universidad de Buenos Aires<br />
+                            ▪ Dictante de cursos de postgrado de la F.O.U.B.A.<br />
+                            ▪ Dictante de cursos de especialidad en el país y en el exterior<br />
+                            ▪ Jurado de concursos para cargos regulares de la Facultad de Odontología de la Universidad de Buenos Aires<br />
+                            ▪ Autora de publicaciones referidas a la especialidad en revistas nacionales y en el exterior<br />
+                            ▪ Participación en campañas de extensión universitaria<br />
+                            ▪ Organizadora de PENN ENDO GLOBAL ARGENTINA 2018<br />
+                            ▪ Dictado de clases y participación académica en la Universidad Complutense de Madrid en el marco de movilidad académica internacional 2018<br />
+                            ▪ Coautora en premios de trabajos de investigación presentados en el país y en el exterior
+                        </p>
+                    </div>
                 </div>
-            </section>
-        </div>
-    )
-}
+            </div>
+        </section>
+    );
+};
