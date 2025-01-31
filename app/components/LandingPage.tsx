7a3e7bd071819690c5425d185217e44e554c7f32
CamilaAznar <aznarcami@gmail.com>
2025-01-31 20:00:43 -0300

    some changes

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -14,7 +14,7 @@ export const LandingPage = () => {
 
   return (
       <div
-        className="bg-cover opacity-80 md:bg-fixed bg-center md:h-[100vh] h-[80vh] mb-10"
+        className="bg-cover opacity-80 md:bg-fixed bg-center md:h-[125vh] h-[80vh] mb-10"
         style={{ backgroundImage: `url(${bgLanding.src})` }}
       >
         <Header />
@@ -28,17 +28,7 @@ export const LandingPage = () => {
                 height={isMobile ? 100 : 130}
               />
             </div>
-            {/* <div className="bg-zinc-700 bg-opacity-60 p-5 text-center rounded-lg">
-              <p className={`text-zinc-100 text-xs md:text-lg mb-1 text-center ${montserrat.className}`}>
-                En nuestro consultorio privado, nos especializamos en brindarte el
-                <span className="font-bold"> mejor cuidado dental</span> con un enfoque personalizado y profesional.
-                Nuestro equipo de odontólogos altamente capacitados utiliza
-                tecnología de vanguardia para garantizarte una experiencia 
-                <span className="font-bold"> cómoda, segura y efectiva</span>.
-                ¡Tené en cuenta que también realizamos Odontopediatría!
-              </p>
-              <p className={`text-zinc-100 mt-5 text-center md:text-base text-xs font-bold mb-3 ${montserrat.className}`}>Od. Laura B Pinasco, Od. Micaela Gonzales Pinasco y Equipo Profesional</p>
-            </div> */}
+            
             <Link href='/contact-us' className="bg-[#00a2af] text-white w-fit md:px-5 px-1 md:py-4 py-2 mt-2 rounded-xl hover:scale-105 duration-200 flex justify-center items-center md:gap-3 gap-1 mx-auto md:mx-0">
               <p className={`text-sm md:text-lg ${montserrat.className}`}>Hacé tu Consulta Acá</p>
               <CiMemoPad size={isMobile ? 20 : 25} />
