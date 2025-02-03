c199cd6253998d668a26d0b81fb330e4003dea98
Federico Herrera <fede.juan.herrera@gmail.com>
2025-02-03 20:08:16 -0300

    adding new team images

--- app/components/CallToHistory.tsx
+++ app/components/CallToHistory.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
-import img from "@/app/img/history/equipo.jpg";
+import img from "@/app/img/history/equipo2.png";
 import { bebas } from "@/app/fonts/fonts";
 
 export const CallToHistory = () => {
   return (
     <div
-      className="flex justify-center items-center md:space-x-4 md:space-y-0 space-y-4 w-full md:h-80 h-64 shadow-md mt-40"
+      className="flex justify-center items-center md:space-x-4 md:space-y-0 space-y-4 w-full md:h-[60vh] h-[40vh] shadow-md mt-40"
       style={{
         backgroundImage: `url(${img.src})`,
         backgroundSize: "cover",
@@ -16,7 +16,7 @@ export const CallToHistory = () => {
     >
       <Link
         href="/history"
-        className="w-fit md:h-auto h-fit md:px-6 px-3 md:py-6 py-2 bg-[#00a2af] bg-opacity-65 text-center rounded-md shadow-xl hover:bg-[#008b96] md:mt-[10%]  mt-[30%]"
+        className="w-fit md:h-auto h-fit md:px-6 px-3 md:py-6 py-2 bg-[#00a2af] bg-opacity-75 text-center rounded-md shadow-xl hover:bg-[#008b96] md:mt-[25%]  mt-[50%]"
       >
         <p
           className={`text-white font-thin md:text-3xl text-xl ${bebas.className}`}
